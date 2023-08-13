d50bba95bba23af196937dda5c4870e6dd789a2b
Einarr07 <aidenperse@gmail.com>
2023-08-13 17:53:29 -0500

    cambio Frontend

--- Backend/src/helpers/crearJWT.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-// Definir la función para generar el token
-const generarJWT = (id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"1d"})
-}
-// Exportación por default de la función
-export default  generarJWT
--- Backend/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './server.js';
-import connection from './database.js';
-
-
-connection()
-// Ejecutar el servidor por medio del puerto
-app.listen(app.get('port'),()=>{
-    console.log(`Servidor activo en http://localhost:${app.get('port')}`);
-})
